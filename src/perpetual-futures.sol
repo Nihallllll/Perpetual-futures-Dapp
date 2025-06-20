23f4eebdf9be61dc3be9ce905f00c8637fdc6cba
Nihallllll <nihal24rajak@gmail.com>
2025-06-20 22:34:46 +0530

    Add contract files

--- src/perpetual-futures.sol
+++ src/perpetual-futures.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Perp {
+contract Perps {
    //this will how a user position will look like 
    struct Position{
-      uint entryPrice;
-      uint leverage;
+      int entryPrice;
+      int leverage;
       uint timestamp;
-      uint margin;
-      uint size;
+      int margin;
+      int size;
       bool isLong;
-      uint quantity;
+      int quantity;
    }
    //address wise user positions 
    mapping(address => Position) userPositions;
@@ -21,19 +21,19 @@ contract Perp {
      Events
 
   */
-   event PositionOpened(address indexed user, bool isLong, uint size, uint quantity, uint entryPrice);
-
+   event PositionOpened(address indexed user, bool isLong, int size, int quantity, int entryPrice);
+   event MarginAdded(address indexed user , bool size , int quantity , int addedMargin);
 
    /*
      
      Functions
 
   */
-   function openPosition(uint margin, uint leverage, bool isLong,uint latestPrice) external {
+   function openPosition(int margin, int leverage, bool isLong,int latestPrice) external {
       require(latestPrice > 0,"Latest Price is not valid");
       require(margin > 0 ,"Not a valid amount");
-      uint positionSize  = margin * leverage ;
-      uint quantity = positionSize / latestPrice;
+      int positionSize  = margin * leverage ;
+      int quantity = positionSize / latestPrice;
       userPositions[msg.sender] = Position({
          entryPrice : latestPrice,
          leverage: leverage,
@@ -42,6 +42,7 @@ contract Perp {
          size : positionSize,
          isLong : isLong,
          quantity : quantity
+         
       });
       emit PositionOpened(msg.sender, isLong, positionSize, quantity, latestPrice);
    }
@@ -50,10 +51,24 @@ contract Perp {
    function getPnL(int latestPrice) external view returns(int amount){
       Position storage userPosition = userPositions[msg.sender];
       if (userPosition.isLong){
-      amount = int(latestPrice - userPosition.entryPrice) * int(userPosition.quantity);
+      amount = latestPrice - userPosition.entryPrice * userPosition.quantity;
       }else{
-      amount = int(userPosition.entryPrice - latestPrice) * int(userPosition.quantity);
+      amount = userPosition.entryPrice - latestPrice * userPosition.quantity;
       }
       return amount ; 
+   }
+
+   function addMargin(int amount ,int latestPrice) external {
+      require(userPositions[msg.sender].size  > 0 ,"No active positions");
+      Position storage userPosition =  userPositions[msg.sender];
+      userPosition.margin += amount;
+      userPosition.size =  userPosition.margin * userPosition.leverage;
+      userPosition.quantity = (userPosition.size * 1e18) / latestPrice ;
+      event MarginAdded(msg.sender, userPosition.size, userPosition.quantity, amount); 
+   }
+
+
+   function removeMargin(int amount , int latestPrice) external {
+      
    }
 }
